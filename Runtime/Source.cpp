6bc47378d7307f60853a95ef2e8288d059dd2cac
Martin Steih <martin.steih@lachmann-rink.de>
2020-12-07 17:18:16 +0100

    worker thread test

--- Runtime/Source.cpp
+++ Runtime/Source.cpp
@@ -1,48 +1,45 @@
-#include <iostream>
+#include <chrono>
 #include <exception>
+#include <iostream>
 #include <memory>
 #include <thread>
-#include <chrono>
 using namespace std::chrono_literals;
 
 #include "DlLoad.hpp"
 
-class Runtime{
-    public:
-        explicit Runtime(std::string libname, std::string plugin_name) : lib{libname}, plugin_name{plugin_name}{
-
-        }
-    void run() {
-        while(true){
-            plugin = lib.GetPlugin(plugin_name);
-            if(!plugin)
-                throw std::exception{};
-            if(plugin->InputCycle)
-                plugin->InputCycle();
-            plugin->Run();
-            if(plugin->OutputCycle)
-                plugin->OutputCycle();
-            std::cout << std::endl;
-            std::this_thread::sleep_for(1s);
-        }
+class Runtime {
+ public:
+  explicit Runtime(std::string libname, std::string plugin_name)
+      : lib{libname}, plugin_name{plugin_name} {}
+  void run() {
+    while (true) {
+      plugin = lib.GetPlugin(plugin_name);
+      if (!plugin) throw std::exception{};
+      if (plugin->InputCycle) plugin->InputCycle();
+      plugin->Run();
+      if (plugin->OutputCycle) plugin->OutputCycle();
+      std::cout << std::endl;
+      std::this_thread::sleep_for(1s);
     }
+  }
 
-    private:
-        DlLoad lib;
-        std::string plugin_name;
-        std::unique_ptr<IPlugin> plugin{nullptr};
+ private:
+  DlLoad lib;
+  std::string plugin_name;
+  std::unique_ptr<IPlugin> plugin{nullptr};
 };
 
-
-auto main(int argc, char** argv)->int{
-    if(argc < 2){
-        exit(-1);
-    }
-    try{
-        Runtime rt{argv[1], argv[2]};
-        rt.run();
-    }catch(std::exception& e){
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
+auto main(int argc, char** argv) -> int {
+  if (argc < 2) {
+    exit(-1);
+  }
+  try {
+    Runtime rt{argv[1], argv[2]};
+    std::thread worker{rt.run()};
+    while (true) std::this_thread::sleep_for(600h);
+    worker.join();
+  } catch (std::exception& e) {
+    std::cerr << e.what() << std::endl;
+  }
+  return 0;
 }
